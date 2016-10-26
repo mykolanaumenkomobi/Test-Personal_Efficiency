4483681db5a58c6516e82352b4df05c69c9941e0
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-10-26 23:23:41 +0300

    Create progressBar.js

---
+++ js/progressBar.js
@@ -0,0 +1,27 @@
+//ProgressBar
+
+function move () {       
+
+    var stop = myTest.iteration *100/(myTest.quest.length),
+	    elem = document.getElementById("Bar"),
+       width = (myTest.iteration - 1) * 100 / (myTest.quest.length);
+
+    if(myTest.iteration ==0){
+    	var sett = 0
+    } else { 
+    	var sett = 12; 
+    };
+
+	var id = setInterval( frame, sett );
+
+    function frame() {
+    	
+       if (width >= stop) {
+           clearInterval(id);
+       } else {
+           width ++; 
+           elem.style.width = width + '%'; 
+       }
+    };
+
+}
