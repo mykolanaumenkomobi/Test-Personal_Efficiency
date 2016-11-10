773c50c632c5579b9565b20fdf0c7b78fb991e27
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-11-10 07:54:15 +0200

    Update progressBar.js

--- js/progressBar.js
+++ js/progressBar.js
@@ -3,16 +3,16 @@
 function move () {       
 
     var stop = myTest.iteration *100/(myTest.quest.length),
-	    elem = document.getElementById("Bar"),
-       width = (myTest.iteration - 1) * 100 / (myTest.quest.length);
+	      elem = document.getElementById("Bar"),
+        width = (myTest.iteration - 1) * 100 / (myTest.quest.length);
 
-    if(myTest.iteration ==0){
-    	var sett = 0
+    if (myTest.iteration ==0) {
+        var sett = 0;
     } else { 
-    	var sett = 12; 
+    	  var sett = 12; 
     };
 
-	var id = setInterval( frame, sett );
+	  var id = setInterval ( frame, sett );
 
     function frame() {
     	
