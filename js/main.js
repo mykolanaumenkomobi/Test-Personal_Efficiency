1171b7598036deeac6afd9793a2a6bb2da225adc
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-11-10 13:05:51 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,3 +29,14 @@ function restartTest(){
 	testRun();
 	
 };
+
+function removeLastChild() {
+
+    var body = document.getElementById("body"),
+        buttonRej = body.lastChild;
+
+	if (buttonRej != null) {
+		body.removeChild(buttonRej);       // удаление выбранного элемета из DOM дерева.
+	};
+
+};
