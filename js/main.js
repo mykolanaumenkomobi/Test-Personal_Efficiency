62d73ccaa39415154b9f6064b2fa1d00c33faec2
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-11-10 07:53:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var myTest = {   //  Creating the One global Object.
 
 	counter: 0,
@@ -10,66 +11,21 @@ var myTest = {   //  Creating the One global Object.
 	    ]
 };
 
-function testRun() {
-    var body = document.getElementsByTagName("body");     
-	var div = document.getElementById("div");
-	    div.className = 'normal';
-		div.innerHTML = myTest.quest [ myTest.iteration ]; //Вывести вопрос iteration  
-
-	var input = document.getElementById("input");
-		document.body.removeChild(input);               //Удалить кнопку "Пройти тест"
-
-    var buttonYes = document.createElement("input");    
-		buttonYes.type = "button";
-		buttonYes.value = "Yes";
-		buttonYes.onclick = pushYes; 
-		buttonYes.style = "border-top-left-radius: 7px; border-bottom-left-radius: 7px;"
-		document.body.appendChild(buttonYes);           //Вставить кнопку Да
-
-	var buttonNo = document.createElement("input");        
-		buttonNo.type = "button";
-		buttonNo.value = "No";
-		buttonNo.onclick = pushNo;
-		buttonNo.style = "border-top-right-radius: 7px; border-bottom-right-radius: 7px;"
-		document.body.appendChild(buttonNo);            //Вставить кнопку Нет 
-};
-		
-function removeLastChild() {
-    var body = document.getElementById("body");
-    var buttonRej = body.lastChild;
-	    if (buttonRej != null) {
-	         body.removeChild(buttonRej);       // удаление выбранного элемета из DOM дерева.
-	    };
-};	
+testRun();
 
 function pushYes () {
 	myTest.counter += 1;    
-    myTest.iteration += 1;
-	if ( myTest.iteration < myTest.quest.length ) {     	  
-	    var div=document.getElementById("div");  
-	    	div.innerHTML = myTest.quest[myTest.iteration ];    //Вывести вопрос iteration			    
-	}
-	else {
-      displayResult (myTest.quest, myTest.counter);             //Вывести кнопку Повторить тест
-	};
-	move (myTest.quest.length, myTest.iteration);
+    questChange ();
 };	 
 
 function pushNo () {
-	myTest.iteration += 1;
-	if ( myTest.iteration < myTest.quest.length ){              //Вывести вопрос iteration
-	   var div = document.getElementById("div");
-		   div.innerHTML = myTest.quest[myTest.iteration ];
-	   }
-	else {                                                      //Вывести кнопку Повторить тест
-	  displayResult (myTest.quest, myTest.counter );
-    }; 
-    move (myTest.quest.length, myTest.iteration);
+	questChange ();
 };
+
 
 function restartTest(){
 	myTest.counter = 0;
 	myTest.iteration = 0;                                            //Удалить результат
 	testRun();
-	move(myTest.quest.length, myTest.iteration)
+	
 };
