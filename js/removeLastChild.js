820ef73f716baf5d8929ae55b5af9b697891b2aa
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-11-10 13:05:34 +0200

    Update removeLastChild.js

--- js/removeLastChild.js
+++ js/removeLastChild.js
@@ -1,11 +1,2 @@
 
-function removeLastChild() {
 
-    var body = document.getElementById("body"),
-        buttonRej = body.lastChild;
-
-	if (buttonRej != null) {
-		body.removeChild(buttonRej);       // удаление выбранного элемета из DOM дерева.
-	};
-
-}
