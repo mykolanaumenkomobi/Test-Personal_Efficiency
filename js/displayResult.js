edff60ddade06cb7cc38cc55e793910e53442cfe
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-11-10 07:53:18 +0200

    Update displayResult.js

--- js/displayResult.js
+++ js/displayResult.js
@@ -1,24 +1,24 @@
 function displayResult() {
+
+	var div = document.getElementById("div");
+		div.className = "big";
+
 	if ( myTest.counter <= myTest.quest.length/3 )	{
-		var div=document.getElementById("div");
-		    div.className = "big";
 	   		div.innerHTML = "Your result is : <strong>Bad</strong>";
 	}
 	else if ( myTest.counter < myTest.quest.length ) {
-		var div = document.getElementById("div");
-		    div.className = "big";
 	   		div.innerHTML = "Your result is : <strong>Good</strong>";
 	}
 	else {
-		var div = document.getElementById("div");
-		    div.className = "big";
 	   		div.innerHTML = "Your result is : <strong>Excelent</strong>";
 	};
-	removeLastChild();                                   //Удалить кнопки Нет   
-    removeLastChild();                                   //Удалить кнопки Да   
+
+	removeLastChild();                                   //Удалить кнопки Нет  из DOM 
+    removeLastChild();                                   //Удалить кнопки Да   из DOM
+    
 	var buttonRestart = document.createElement("input"); //Вставить кнопку Повторить тест
 	    buttonRestart.type = "button";
-		buttonRestart.value = "Pass the test once again";
+		buttonRestart.value = "Once again";
 		buttonRestart.id = "input";
 		buttonRestart.onclick = restartTest;
 		document.body.appendChild(buttonRestart);	
