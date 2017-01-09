c77d15a8e01e918aabfe6c1eaa8d468375e3e869
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-01-09 04:06:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,118 @@
 
-var myTest = {   //  Creating the One global Object.
-
+var Module = {  
 	counter: 0,
-	iteration: 0, 
 	quest: [
 		"Do you read the books?",
 		"Do you study English?",
 		"Do you do the morning exercises?",
 		"Do you go to bed early?"
 	    ]
-};
 
-testRun(iteration);
-
-function pushYes () {
-	myTest.counter += 1;    
-    questChange ();
-};	 
-
-function pushNo () {
-	questChange ();
 };
 
 
-function restartTest(){
-	myTest.counter = 0;
-	myTest.iteration = 0;                                            //Удалить результат
-	testRun();
+function $ (ID) {
+	return document.getElementById(ID);
+};
+
+Module.text = $("message");
+Module.len = Module.quest.length;
+initDomElement ();
+move(Module.len);
+
+function initDomElement () {
+
+    Module.button = $("buttonInit");
+	Module.buttonYes = $("Yes");
+	Module.buttonNo = $("No");
+
+	if (Module.button) {
+		Module.step = 0;
+		Module.button.onclick = goTest; 
+	    //console.log("Init Button");
+	} else if (Module.buttonNo) {
+		Module.buttonNo.onclick = pressNo;
+		//console.log("Init Button No");
+	} else if (Module.buttonYes) {
+		Module.buttonYes.onclick = pressYes; 
+		//console.log("Init Button Yes");
+	};
+};
+
+Module.container = $("container");
+console.log(Module.div);
+
+
+function goTest () {
+
+	console.log("counter "+ Module.counter);
+    console.log ("step "+ Module.step);
+	if (Module.step == 0) {       
+		//console.log("Module.step == 0");                 
+		container.removeChild(Module.button);
+	    buttonCreation ("Yes");
+	    buttonCreation ("No");
+	    move(0);
+
+	};
+
+	if (Module.step < Module.len) {                             //Проверяем не закончились ли вопросы в массиве
+	    //console.log("Module.step < Module.quest.length"); 
+		Module.text.innerHTML = Module.quest[Module.step];      //Вставляем текст вопроса в разметку
+	    Module.step ++;
+	} else {
+		  Module.step ++;
+          //console.log(Module.counter);
+		  container.removeChild(Module.buttonNo);
+		  container.removeChild(Module.buttonYes);
+		  displayResult (Module.len);
+		  Module.counter = 0;
+	      setTimeout ( createRestartButton , 1300);
+	};
+
+	move(Module.step-1, Module.len);
+};
+
+function buttonCreation (name, id) {
+	//console.log(id);
+	var button = document.createElement("input");
+	button.value = name;
+	button.type = "button";
+	(id == undefined) 
+	  ? (id = name, 
+	     button.className = "control button")
+	    :button.className = "big button";
+	button.id = id;
+	container.appendChild(button);
+
+	initDomElement ();
+	id = undefined;
+};
+
+
+function pressYes () {
+	Module.counter ++;
+	goTest ();
+	//console.log("Press YES");
+};
+
+
+function pressNo () {
+	goTest ();
+	//console.log("Press NO");
+};
+
+
+function displayResult (len) {
+	//console.log(Module.counter);
+	var text = $("message");
+	( Module.counter <= len/3 )	? text.innerHTML = "Your result is : <strong>Bad</strong>!" : 
+	( Module.counter < len ) ?text.innerHTML = "Your result is : <strong>Good</strong>!"
+	:text.innerHTML = "Your result is : <strong>Excelent</strong>!";
+	
 	
 };
 
-function settings () {
-	if (this.value = "on") {
-		console.log("Ok!");
-	}
+function createRestartButton () {
+		buttonCreation ("Once again", "buttonInit")
 };
